b115a08178b36710b5e3ff0509833e5f03097441
bhbbereket <bhbbereket@gmail.com>
2022-09-23 13:49:27 -0500

    initial commmit:

---
+++ src/main/java/com/sa/finalProject/App.java
@@ -0,0 +1,13 @@
+package com.sa.finalProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
