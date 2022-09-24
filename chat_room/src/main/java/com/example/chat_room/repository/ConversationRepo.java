accf25f894343f84081eb794d48c87d82eca2009
bhbbereket <bhbbereket@gmail.com>
2022-09-24 18:29:56 -0500

    messages

--- chat_room/src/main/java/com/example/chat_room/repository/ConversationRepo.java
+++ chat_room/src/main/java/com/example/chat_room/repository/ConversationRepo.java
@@ -2,8 +2,13 @@ package com.example.chat_room.repository;
 
 import com.example.chat_room.model.Conversation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ConversationRepo extends MongoRepository<Conversation, String> {
+//    @Query(value = "")
+//    List<Conversation> findBySender(String id);
 }
--- chat_room/src/main/java/com/example/chat_room/service/Service.java
+++ chat_room/src/main/java/com/example/chat_room/service/Service.java
@@ -1,9 +1,15 @@
 package com.example.chat_room.service;
 
 import com.example.chat_room.model.Conversation;
+import com.example.chat_room.model.Message;
 import com.example.chat_room.repository.ConversationRepo;
 import com.example.chat_room.repository.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
 
 @org.springframework.stereotype.Service
 public class Service {
@@ -11,7 +17,20 @@ public class Service {
     private ConversationRepo conversationRepo;
     @Autowired
     private MessageRepo messageRepo;
+    @Autowired
+    private MongoTemplate template;
     public void addConverstion(Conversation conversation){
         conversationRepo.save(conversation);
     }
+    private List<Conversation> getConversation(String id){
+        Query query=new Query();
+        query.addCriteria(Criteria.where("members").in(id));
+        return template.find(query,Conversation.class);
+    }
+    private List<Message> getMessage(String id){
+        Query query=new Query();
+        query.addCriteria(Criteria.where("con_id").is(id));
+        return template.find(query,Message.class);
+    }
+
 }
